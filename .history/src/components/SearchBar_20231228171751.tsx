1d217f16dafecf8122650554f9a0d1e256f4bae2
Fortivera <ant97str@gmail.com>
2023-12-28 17:23:00 -0500

    set the size for search bar

--- .history/src/components/SearchBar_20231228171751.tsx
+++ .history/src/components/SearchBar_20231228171751.tsx
@@ -1,7 +1,7 @@
 export default function SearchBar() {
     return (
         <form className="flex items-center">
-            <div className="relative w-full">
+            <div className="relative w-full h-44">
                 {/* Search Icon */}
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3">
                     <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -13,7 +13,7 @@ export default function SearchBar() {
                 <input
                     type="search"
                     id="default-search"
-                    className="block w-full h-10 pl-10 pr-20 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block w-full pl-10 pr-20 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search Mockups, Logos..."
                     required
                 />
